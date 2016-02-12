350926227fab594dbc9cbf9881f5fd830d0c636d
Sam Felton <sam.cfelton@gmail.com>
2016-02-12 14:23:41 -0500

    First "functional" versionjk:w

---
+++ libtodo.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+#import os
+from os.path import isfile
+
+#--------[ Classes ]-----------------------------------------------------------
+class Task(object):
+
+    def __init__(self, desc):
+        self.completed   = False
+        self.description = desc
+
+    def __str__(self):
+        if self.completed:
+            return "[X] {}".format(self.description)
+        else:
+            return "[ ] {}".format(self.description)
+
+#--------[ Methods ]-----------------------------------------------------------
+def read_tasks_from_file(filename):
+    if isfile(filename):
+        tasks = []
+        with open(filename, encoding='utf-8') as f:
+            for line in f:
+                #The first characters of line will tell what it is
+                char_key = line[0:2]
+                if char_key[0]  == '[':
+                    temp_task = Task("Placeholder Description")
+                    temp_task.completed = True if line[1] == 'X' else False
+                    temp_task.description=line[4:-1]
+                    tasks.append(temp_task)
+                elif char_key == "GP":
+                    pass
+                    #PLACEHOLDER for GROUPS
+        return tasks
+    else:
+        print("No todo file found!")
+        print("Creating todo file at " + filename)
+        create_file = open(filename, mode="w")
+        create_file.close()
+
+def write_tasks_to_file(tasks, filename):
+    with open(filename, mode="w", encoding='utf-8') as f:
+        for t in tasks:
+            f.write(t.__str__())
+            f.write('\n')
+
+def print_tasks(tasks):
+    print("--------TODO List--------")
+    for t in tasks:
+        print(t)
+
+def print_numbered_tasks(tasks):
+    print("--------TODO List--------")
+    ctr = 1;
+    for t in tasks:
+        print("({:>2}) {}".format(ctr, t))
+        ctr += 1
+
--- todo.py
+++ todo.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import libtodo as lib
+from libtodo import Task
 
+#--------[ GLOBALS ]-----------------------------------------------------------
+VERSION     = "0.1"
+FILENAME    = "todo.txt"
+TODO_DIR    = os.path.dirname(os.path.realpath(sys.argv[0]))
+TODO_FILE   = str(TODO_DIR+"/"+FILENAME)
 
 def Usage():
     print('''
@@ -11,24 +19,55 @@ usage: todo [action] [argument]
 
 actions:
 a | add     <task>      Add a task to todo list
-f | finish  <task ID>   Finish a tak
+f | finish  <task ID>   Finish a task
 h | help                Print help
 l | list                List tasks
 v | version             Print todo version
 ''')
 
 def Version():
-    print("Version")
+    print("todo v{}".format(VERSION))
 
 def add_task():
-    print("Add task")
+    desc=input("Task description: ")
+    temp_task = Task(desc)
+    with open(TODO_FILE, mode='a', encoding='utf-8') as f:
+        f.write(temp_task.__str__())
+        f.write('\n')
+
+def delete_task():
+    tasks = lib.read_tasks_from_file(TODO_FILE)
+    lib.print_numbered_tasks(tasks)
+    num=int(input("Which task would you like to delete? "))
+    if (num < 1) or (num > len(tasks)):
+        print("Task number doesn't exist")
+        exit()
+    del tasks[num-1]
+    lib.write_tasks_to_file(tasks, TODO_FILE)
 
 def finish_task():
-    print("Finish task")
+    tasks = lib.read_tasks_from_file(TODO_FILE)
+    lib.print_numbered_tasks(tasks)
+    num=int(input("Which task did you complete? "))
+    if (num < 1) or (num > len(tasks)):
+        print("Task number doesn't exist")
+        exit()
+    tasks[num-1].completed=True
+    lib.write_tasks_to_file(tasks, TODO_FILE)
+
+def unfinish_task():
+    tasks = lib.read_tasks_from_file(TODO_FILE)
+    lib.print_numbered_tasks(tasks)
+    num=int(input("Which task did you complete? "))
+    if (num < 1) or (num > len(tasks)):
+        print("Task number doesn't exist")
+        exit()
+    tasks[num-1].completed=False
+    lib.write_tasks_to_file(tasks, TODO_FILE)
 
 def list_tasks():
-    print("List Tasks")
-
+    tasks = lib.read_tasks_from_file(TODO_FILE)
+    lib.print_tasks(tasks)
 
 
 """
@@ -39,17 +78,21 @@ if len(sys.argv) == 1:
     exit()
 
 argument_parser = { "a" : add_task,
+                    "d" : delete_task,
                     "f" : finish_task,
                     "h" : Usage,
                     "l" : list_tasks,
+                    "u" : unfinish_task,
                     "v" : Version,
 
                     #Long actions
                     "add"   : add_task,
+                    "delete": delete_task,
                     "finish": finish_task,
                     "help"  : Usage,
                     "list"  : list_tasks,
                     "version": Version }
 
-argument_parser[sys.argv[1]]()
+#Check the first argument and default to Usage()
+argument_parser.get(sys.argv[1], Usage)()
 
